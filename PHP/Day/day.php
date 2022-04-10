c386f69e32b6da3f2af2da9927a1c2cf0e69bed6
Jose Jimmy <josejimmy8055@gmail.com>
2022-04-10 22:42:03 +0530

    day

---
+++ PHP/Day/day.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+    <?php
+    $var = 5;
+    switch ($var) {
+        case 1:
+            echo "the day is sunday";
+            break;
+        case 2:
+            echo "the day is monday";
+            break;
+        case 3:
+            echo "the day is tuesday";
+            break;
+        case 4:
+            echo "the day is wednesday";
+            break;
+        case 5:
+            echo "the day is thursday";
+            break;
+        case 6:
+            echo "the day is friday";
+            break;
+        case 7:
+            echo "the day is saturday";
+            break;
+    }
+    ?>
+</body>
+
+</html>
